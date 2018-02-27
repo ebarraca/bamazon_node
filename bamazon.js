68c3d56d912dae7d6ce898f3efdc6dc1c1f7f319
Erica Barraca <ericabarraca@dhcp-10-105-190-129.wireless.northwestern.private>
2018-02-26 18:18:44 -0600

    input updated

--- bamazon.js
+++ bamazon.js
@@ -22,42 +22,44 @@ function start(){
     connection.query('SELECT * FROM products', function(err, res){
         if (err) throw err;
 
-    for(var i = 0; i<res.length;i++){
-    console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
-    console.log('--------------------------------------------------------------------------------------------------')
-  }
+        for(var i = 0; i<res.length;i++){
+            console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+            console.log('--------------------------------------------------------------------------------------------------')
+        }
+        userInputId(res);
     })
-
 }
 start();
 
-    function userInputId() {
+    function userInputId(res) {
       inquirer
         .prompt({
           name: "id",
           type: "input",
           message: "What is the ID of the item you'd like to buy?"
         })
-        .then(function(input) {
-        //user types answer //
+        .then(function(userResponse) {
+            for(var i = 0; i<res.length;i++){
+                if (res.id==userResponse.id){
+                    userInputQty(userResponse.id);
+                }
+
+            }
         })
     };
-    userInputId();
 
-    //
-    //
-    // function userInputQty() {
-    //   inquirer
-    //     .prompt({
-    //       name: "id",
-    //       type: "input",
-    //       message: "How many would you like to buy?"
-    //     })
-    //     .then(function(input) {
-    //     //user types answer //
-    //     //check database to see if there is enough
-    //     //if not enough - INSUFFICIENT
-    //     //if there is enough, then update the SQL DB to reflect new QTY
-    //     })
-    // };
-    // userInputQTY();
+    function userInputQty(userResponseId) {
+      inquirer
+        .prompt({
+          name: "id",
+          type: "input",
+          message: "How many would you like to buy?"
+        })
+        .then(function(input) {
+        //user types answer //
+        //check database to see if there is enough
+        //if not enough - INSUFFICIENT
+        //if there is enough, then update the SQL DB to reflect new QTY
+        })
+    };
+    userInputQTY();
