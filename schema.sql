f461ab8e0d4658fee8e1e3c40d31989950721a3b
Erica Barraca <ericabarraca@dhcp-10-105-199-59.wireless.northwestern.private>
2018-03-19 21:52:13 -0500

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,15 @@
 
+CREATE DATABASE bamazon_db;
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Echo", "electronics", 99, 64);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Echo Dot", "electronics", 49, 67);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Obama: An Intimate Portrait", "books", 31, 4);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Harry Potter Book Collection", "books", 57, 90);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Nest Theromstat", "home", 199, 23);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("GoPro Hero5", "cameras", 298, 8);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Kong", "pets", 6, 27);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Nintendo Switch", "video games", 299, 17);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Mario Kart", "video games", 59, 52);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Fire TV Stick", "electronics", 39, 22);
+USE bamazon_db;
+
+CREATE TABLE products(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(200) NOT NULL,
+  department_name VARCHAR(200) NOT NULL,
+  price INTEGER (11) NOT NULL,
+  stock_quantity INTEGER (11) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
